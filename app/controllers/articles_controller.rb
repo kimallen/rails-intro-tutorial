a7da20c92df176fb588ff17dc63f3fe4b36286a2
kimallen <kimmarieallen@gmail.com>
2015-12-27 21:42:43 -0800

    Add blog2 to empty github repository

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,14 @@
+class ArticlesController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  	@article = Article.new(params[:article])
+
+  	@article.save
+  	redirect_to @article
+  end
+end
